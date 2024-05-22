7d770906f3d3d50ca4b88ac9a0e4553d82b1da8c
IgorPolunichev <93341324+IgorPolunichev@users.noreply.github.com>
2024-05-22 23:45:48 +0300

    тест коммит 1

--- src/main/java/com/example/tradeintechniqueapp/dto/actsDto/ActFrontPageDto.java
+++ src/main/java/com/example/tradeintechniqueapp/dto/actsDto/ActFrontPageDto.java
@@ -1,17 +1,16 @@
 package com.example.tradeintechniqueapp.dto.actsDto;
+
+import com.example.tradeintechniqueapp.database.entity.Work;
+import com.example.tradeintechniqueapp.database.entity.audit.ActPay;
+import com.example.tradeintechniqueapp.dto.machinesDto.MachineReadDto;
+import com.example.tradeintechniqueapp.dto.usersDto.UserReadDto;
 import lombok.Value;
+
 import java.time.LocalDate;
-/*
-* Добавили строку
-* Добавили еще одну.
-* */
+import java.util.List;
+
 @Value
 public class ActFrontPageDto {
-    /*
-    * А мы добавили вот тут
-    * И вот тут
-    * */
-
     Long id;
     LocalDate date;
     String number;
